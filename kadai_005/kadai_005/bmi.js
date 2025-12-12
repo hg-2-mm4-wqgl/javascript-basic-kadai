023ce734abb986edfed6044121cd96b44c37da99
hg-2-mm4-wqgl <higu-2-mm4@hotmail.co.jp>
2025-12-12 23:05:39 +0900

    kadai_005を再々アップロード

--- kadai_005/kadai_005/bmi.js
+++ kadai_005/kadai_005/bmi.js
@@ -1,14 +1,11 @@
-// 変数の宣言
-let bodyWeight;
+// 体重（kg)を変数に代入
+let bodyWeight = 68;
 
-// 値の代入
-bodyWeight = 68;
+// 身長（m）を変数に代入
+let height = 1.7;
 
-//　変数の宣言
-let height;
+// BMIを計算
+let bmi = bodyWeight / (height * height);
 
-// 値の代入
-height = 1.7;
-
-// 四則演算
-console.log(bodyWeight / (height * height))
+//　BMIの値をコンソールに出力
+console.log(bmi);
